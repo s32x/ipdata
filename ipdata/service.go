c91091ce02157a54763a52cdbdb287f8fe017f8b
Steven Wolfe <steven@swolfe.me>
2018-12-02 01:44:34 -0700

    Adding root redirect

---
+++ ipdata/service.go
@@ -0,0 +1,38 @@
+package ipdata /* import "s32x.com/ipdata/ipdata" */
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+// Start starts the ipdata API service using the passed port for binding the
+// HTTP server to
+func Start(port string) {
+	// Create the ipdata client
+	ic, err := NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ic.Close()
+
+	// Create a new echo Echo and bind middleware
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	// Bind all API endpoint handlers
+	e.GET("/", func(c echo.Context) error {
+		return c.Redirect(http.StatusTemporaryRedirect, "/lookup/"+c.RealIP())
+	})
+	e.GET("/lookup/:ip", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, ic.Lookup(c.Param("ip")))
+	})
+
+	// Listen on the passed port
+	e.Logger.Fatal(e.Start(":" + port))
+}
--- main.go
+++ main.go
@@ -1,51 +1,12 @@
 package main /* import "s32x.com/ipdata" */
 
 import (
-	"log"
-	"net/http"
 	"os"
-	"strconv"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 	"s32x.com/ipdata/ipdata"
 )
 
-var (
-	// The port that the service will bind to
-	port = getenv("PORT", "8080")
-	// Whether or not to serve the web frontend
-	web, _ = strconv.ParseBool(getenv("WEB", "false"))
-)
-
-func main() {
-	// Create a new echo Echo and bind middleware
-	e := echo.New()
-	e.HideBanner = true
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(middleware.CORS())
-
-	// Create the ipdata client
-	ic, err := ipdata.NewClient()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ic.Close()
-
-	// Bind all API endpoint handlers
-	e.GET("/lookup/:ip", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, ic.Lookup(c.Param("ip")))
-	})
-
-	// TODO Build a basic front-end interface
-	// // Serve the static web content
-	// e.Static("/", "web")
-	// e.Static("/assets", "web/assets")
-
-	// Listen on the passed port
-	e.Logger.Fatal(e.Start(":" + port))
-}
+func main() { ipdata.Start(getenv("PORT", "8080")) }
 
 // getenv retrieves a variable from the environment and falls back to a passed
 // default value if the key doesn't exist
